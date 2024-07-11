d03e8b6977bae5e7cf4d7e53bec2ddbec82c28a4
Saron <114290476+saron03@users.noreply.github.com>
2024-07-11 18:44:25 +0300

    Time: 141 ms (58.33%) | Memory: 17.1 MB (36.09%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -1,10 +1,27 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        output = [[], []]
-        for i in nums1:
-            if i not in nums2 and i not in output[0]:
-                output[0].append(i)
-        for j in nums2:
-            if j not in nums1 and j not in output[1]:
-                output[1].append(j)  
-        return output
+        hashtable = {}
+        first_true_second_false = []
+        second_true_first_false = []
+        for num in nums1:
+            if num not in hashtable:
+                hashtable[num] = [True,False]
+        for num in nums2:
+            if num in hashtable:
+                hashtable[num][1] = True
+            else:
+                hashtable[num] = [False,True]
+        for key, value in hashtable.items():
+            if value[0] and not value[1]:
+                first_true_second_false.append(key)
+            elif not value[0] and value[1]:
+                second_true_first_false.append(key)
+        return [first_true_second_false,second_true_first_false]
+        
+        
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        
