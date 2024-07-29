eee52399342f2d7a366885a1079d8379ae11db44
Saron <114290476+saron03@users.noreply.github.com>
2024-07-29 13:12:00 +0300

    Time: 30 ms (96.91%) | Memory: 16.6 MB (43.76%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,7 +1,5 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        pointer = 0
-        for i in range(m, m + n):
-            nums1[i] = nums2[pointer]
-            pointer  +=1
+        for i in range(n):
+            nums1[m +i] = nums2[i]
         nums1.sort()
