e5dd19b8b1a472d185ac39266e3acb29090e3371
Saron <114290476+saron03@users.noreply.github.com>
2024-08-15 01:08:22 +0300

    Time: 210 ms (46.74%) | Memory: 17.8 MB (74.26%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -3,4 +3,4 @@ class Solution:
         if len(nums) == 3:
            return math.prod(nums)
         nums.sort()
-        return max(math.prod(nums[-1:-4:-1]), math.prod(nums[0:3]), nums[0]*nums[1]*nums[-1])
+        return max(math.prod(nums[-1:-4:-1]), nums[0]*nums[1]*nums[-1])
