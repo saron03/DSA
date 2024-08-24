a7ef4033161b79c0d813c33a5a6c7509141563b5
Saron <114290476+saron03@users.noreply.github.com>
2024-08-24 20:09:56 +0300

    Time: 362 ms (15.08%) | Memory: 17.1 MB (32.13%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -3,25 +3,13 @@ class Solution(object):
         people.sort()
         count = 0
         left, right = 0, len(people) - 1
-        while left < right:
-            if people[right] == limit:
-                count +=1
-                right -=1
-            elif people[left] + people[right] == limit:
-                count +=1
-                left +=1
-                right-=1
-            elif people[left] + people[right] > limit:
-                right -=1
-                count +=1
-            else:
-                count +=1
-                left +=1
-                right -=1
-        if left == right:
-            count +=1
+
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            count += 1
+
         return count
 
-
-
-        
+            
