fa260951cde8c5490c07865c98647f2ab2d843b4
Saron <114290476+saron03@users.noreply.github.com>
2024-08-27 17:26:59 +0300

    Time: 896 ms (38.61%) | Memory: 28.3 MB (55.92%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -3,8 +3,6 @@ class Solution:
         n = len(nums)
         run_sum = sum(nums[:k])
         run_average = run_sum /k
-        
-        run_average = max(run_average, run_sum /k)
         for i in range(1,n-k+1):
             run_sum = run_sum - nums[i-1] + nums[i + k-1] 
             run_average = max(run_average, run_sum /k) 
