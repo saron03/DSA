31a3f6cdd039faecc76f2e8bdefde7616ebfd888
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-02-21 10:27:43 +0300

    Time: 23 ms (86.90%) | Memory: 34.7 MB (20.47%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        maxNum = float('-inf')
+        for i in nums:
+            if i > maxNum:
+                maxNum = i
+        if maxNum < 0:
+            return 1
+        nums = set(nums)
+        for i in range(1,maxNum+1):
+            if i not in nums:
+                return i
+        return maxNum +1
