fcc842cde22e29f463bdf89eec8434fe238e4b31
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-03-09 22:21:19 +0300

    Time: 759 ms (65.22%) | Memory: 18.8 MB (78.26%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numberOfAlternatingGroups(self, colors, k):
+        for i in range(k - 1):
+            colors.append(colors[i])
+
+        length = len(colors)
+        result = 0
+        # Initialize the bounds of the sliding window
+        left = 0
+        right = 1
+
+        while right < length:
+            # Check if the current color is the same as the last one
+            if colors[right] == colors[right - 1]:
+
+                # Pattern breaks, reset window from the current position
+                left = right
+                right += 1
+                continue
+
+            # Extend window
+            right += 1
+
+            # Skip counting sequence if its length is less than k
+            if right - left < k:
+                continue
+
+            # Record a valid sequence and shrink window from the left to search for more
+            result += 1
+            left += 1
+
+        return result
