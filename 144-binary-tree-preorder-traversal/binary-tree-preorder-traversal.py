b193ca3be22b356381c65c0c3335887f0556ce95
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-04-25 14:32:11 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.84%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -8,11 +8,10 @@ class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res=[]
         def preorder(root):
-            if not root:
-                return
-            res.append(root.val)
-            preorder(root.left)
-            preorder(root.right)
+            if root:
+                res.append(root.val)
+                preorder(root.left)
+                preorder(root.right)
         preorder(root)
         return res
     
