13b8a9b8522d11bd67b6d27c6415e4d2ad492871
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-10-13 13:49:37 +0300

    Time: 23 ms (21.66%) | Memory: 18.2 MB (7.44%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        letter_counts = {}
+        for word in words:
+            letter_counts[word] = Counter(word)
+        
+        res = []
+
+        i,j = 0,1
+        while j < len(words):
+            if letter_counts[words[j]] == letter_counts[words[i]]:
+                j +=1
+            else: 
+                res.append(words[i])
+                i = j
+                j +=1
+        res.append(words[i])
+
+        return res
+
+
+
+
+
