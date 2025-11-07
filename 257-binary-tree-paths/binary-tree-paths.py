2a4055f1c52ae29a4d6229fb8451397e4016bdb6
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-11-07 23:23:54 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.35%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -1,21 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        def traversal(root, path):
-            if not root:
+        ans = []
+
+        def dfs(node, path):
+            if node is None:
                 return
-            if not root.left and not root.right:
-                arr.append("->".join(path))
-            if root.left:
-                traversal(root.left, path + [str(root.left.val)])
-            if root.right:
-                traversal(root.right,path + [str(root.right.val)])
-        arr = []
-        traversal(root,[str(root.val)])
-        return arr
+
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                ans.append(path)
+                return
+            path += "->"
+            dfs(node.left, path)
+            dfs(node.right, path)
             
+        dfs(root, "")
+        return ans
