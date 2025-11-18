853fc5b611a13f3145a138aab8cdca72eba9536a
Saron Yitbareck <114290476+saron03@users.noreply.github.com>
2025-11-18 12:03:40 +0300

    Time: 209 ms (16.23%) | Memory: 18 MB (98.80%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -6,8 +6,9 @@ class Solution:
             min_ind = i
             for j in range(i+1,n):
                 if heights[j] > heights[min_ind]:
-                    heights[j],heights[min_ind] = heights[min_ind],heights[j]
-                    names[j], names[min_ind] = names[min_ind], names[j]
+                    min_ind = j
+            heights[i],heights[min_ind] = heights[min_ind],heights[i]
+            names[i], names[min_ind] = names[min_ind], names[i]
         return names
 
 
